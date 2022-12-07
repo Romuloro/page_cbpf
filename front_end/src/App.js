f2c2c43836166d1ff89628f5890fbe4d7f24b4e5
RÔMULO RODRIGUES DE OLIVEIRA <rrorodriguesoliveira@gmail.com>
2022-12-07 13:51:17 -0300

    feat: initial page

---
+++ front_end/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Classe from "./Components/Classe";
+import ListaArquivo from "./Components/Lista_arquivo";
+
+import Titulo from "./Components/Titulo"
+
+export default function App() {
+  return (
+    <div>
+      <Titulo/>
+      <Classe/>
+      <ListaArquivo/>
+    </div>
+  );
+}
---
+++ front_end/src/Components/Classe/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Classe = () => {
+  return (
+    <div>
+      <label>
+        <input type="radio" value="option1"/>
+        Option 1
+      </label>
+      <label>
+        <input type="radio" value="option2"/>
+        Option 2
+      </label>
+      <label>
+        <input type="radio" value="option3"/>
+        Option 3
+      </label>
+    </div>
+  );
+};
+
+export default Classe;
