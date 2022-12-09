c4432bd259501414d3bc15df6b4ad52a462beb5b
RÔMULO RODRIGUES DE OLIVEIRA <rrorodriguesoliveira@gmail.com>
2022-12-08 22:29:18 -0300

    feat: update file python back end

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -7,6 +7,8 @@ import Titulo from "./Components/Titulo"
 import FileUpload from "./Components/FileUpload";
 import FileList from "./Components/FileList";
 
+import uploadArq from "./api/api"
+
 import './App.css'
 
 export default function App() {
@@ -16,11 +18,26 @@ export default function App() {
     setFiles(files.filter((file) => file.name !== filename));
   };
 
+  const dowloadFile = async (event) => {
+    const file = event.target.files[0];
+    const formData = new FormData();
+    formData.append("newFile", file, file.name);
+
+    axios.post("http://localhost:8080/arquivos", formData);
+  }
+
   return (
     <div className="first">
       <div className="container">
         <Titulo />
         <Classe />
+        {/* <form   action="http://localhost:8080/arquivos" 
+                method="POST" 
+                enctype="multipart/form-data">
+            <label for="meuArquivo">Upload Arquivo</label>
+            <input type="file" id="meuArquivo" name="meuArquivo"/>
+            <input type="submit" value="Submit"/>
+        </form> */}
         {/* <ListaArquivo /> */}
         {/* <Resultado /> */}
         <FileUpload files={files} setFiles={setFiles} removeFile={removeFile} />
--- front_end/src/Components/FileUpload/index.js
+++ front_end/src/Components/FileUpload/index.js
@@ -1,48 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlus } from "@fortawesome/free-solid-svg-icons";
 import "./FileUpload.css";
-import axios from "axios";
+
+import { uploadArq } from "../../api/api";
 
 const FileUpload = ({ files, setFiles, removeFile }) => {
-  const uploadHandler = (event) => {
-    const file = event.target.files[0];
-    console.log(event.target.files[0]);
-    if (!file) return;
-    file.isUploading = true;
-    setFiles([...files, file]);
 
-    // upload file
-    const formData = new FormData();
-    formData.append("newFile", file, file.name);
-    axios
-      .post("http://localhost:8080/upload", formData)
-      .then((res) => {
-        file.isUploading = false;
-        setFiles([...files, file]);
-      })
-      .catch((err) => {
-        // inform the user
-        console.error(err);
-        removeFile(file.name);
-      });
+  const [arquivo, setArquivo] = useState([])
+ 
+  const uploadFiles = async (e) => {
+    e.preventDefault();
+    console.log("aqui");
+    console.log(arquivo);
+    await uploadArq(arquivo);
   };
 
   return (
     <>
       <div className="file-card">
-        <div className="file-inputs">
-          <input type="file" onChange={uploadHandler} multiple={true} />
-          <button>
-            <i>
-              <FontAwesomeIcon icon={faPlus} />
-            </i>
-            Upload
-          </button>
-        </div>
-
-        <p className="main">Supported files</p>
-        <p className="info">JPG, PNG</p>
+        <form
+          onSubmit={uploadFiles}
+        >
+          <div className="file-inputs">
+            <input
+              type="file"
+              id="meuArquivo"
+              name="arquivo"
+              onChange={e => setArquivo(e.target.files[0])}
+              multiple={true}
+            />
+            <button type="submit">
+              <i>
+                <FontAwesomeIcon icon={faPlus} />
+              </i>
+              Upload
+            </button>
+          </div>
+          <p className="main">Supported files</p>
+          <p className="info">JPG, PNG</p>
+          <input type="submit" value="Submit" />
+        </form>
       </div>
     </>
   );
